36883d242aecfc6624e2eda31c67306f802db406
Alexander Urizar <urizaraj@gmail.com>
2018-03-15 18:34:27 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,10 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def search(string)
+    Student.all.find_all do |student|
+      student.name.include?(string)
+    end
+  end
 end
